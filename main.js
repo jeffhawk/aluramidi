556e8dddda55064bafb593c5f7765356282a0ec2
jeffhawk <46302791+jeffhawk@users.noreply.github.com>
2022-11-27 13:27:51 -0300

    quase

---
+++ main.js
@@ -0,0 +1,35 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeSons = document.querySelectorAll('audio');
+let contador = 0;
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+//const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+    tecla.onblur = function(){
+        tecla.classList.remove('ativa');
+    }
+}
